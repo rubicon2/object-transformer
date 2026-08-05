d27ae00b87d243a1101d38fb284097f4dd949f04
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-08-05 16:31:36 +0100

    eslint.config: set up to work with typescript
    Previously, linter was showing problems where
    typescript syntax was being used in tests.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,29 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
-import vitest from '@vitest/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import { jsdoc } from 'eslint-plugin-jsdoc';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: { globals: globals.node },
   },
+  // This has to be before the overrides.
+  tseslint.configs.recommended,
   {
-    files: ['**/*.test.*'],
-    plugins: {
-      vitest,
-    },
+    files: ['**/*.{ts,mts}'],
     rules: {
-      ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          caughtErrors: 'none',
+        },
+      ],
     },
   },
-  jsdoc({
-    config: 'flat/recommended',
-  }),
   eslintConfigPrettier,
 ]);
